73e82f1984cf24d5373b77b60d9136d879bf61cc
Evgeny Bondarenko <evgeny.bondarenko@onetwotrip.com>
2018-01-17 02:06:08 +0300

    better install script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 cd docs
+chmod 770 tmp
+./composer.phar update
 ./composer.phar install
 cd vendor/phpmyadmin/phpmyadmin
+../../../composer.phar update
 ../../../composer.phar install
 cd ../../../
 cp ./phpMyAdmin/config.inc.php vendor/phpmyadmin/phpmyadmin/config.inc.php
